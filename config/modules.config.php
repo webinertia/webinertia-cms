f417d14df228fe1661e1aaa3680f660a4488a240
Joey Smith <jsmith@webinertia.net>
2023-06-18 17:22:31 -0500

    Adding lock file removed build-theme from post-install-cmd since theme is now when the project is cloned Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,7 +6,28 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Laminas\Navigation',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\I18n',
+    'Laminas\I18n',
+    'Laminas\Mail',
+    'Laminas\Log',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Db',
     'Laminas\Router',
     'Laminas\Validator',
+    'Laminas\DeveloperTools',
+    'Webinertia\Filter',
+    'Webinertia\Validator',
+    'Limatus',
+    'Webinertia\Session',
+    'Webinertia\ThemeManager',
     'Application',
 ];
