7592f95376fd25e16aff997d038c297fede953d5
Joey Smith <jsmith@webinertia.net>
2023-06-18 15:19:45 -0500

    adding theme.config.php Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

---
+++ data/thememanager/theme.config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'default' => [
+        'id' => 1,
+        'active' => true,
+        'name' => 'default',
+        'fallback' => 'default',
+    ],
+    'dark' => [
+        'id' => 2,
+        'active' => false,
+        'name' => 'dark',
+        'fallback' => 'default',
+    ],
+];
